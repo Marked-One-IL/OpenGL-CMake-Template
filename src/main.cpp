b0ebaaac3c6e8a849c1b73ed6541bd514531a595
Marked-One <strelokoneil@gmail.com>
2026-08-23 16:03:36 +0300

    Included all the libs and added extra comments about that. This is because the including is not that straightforward.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <filesystem>
 
+#define STB_IMAGE_IMPLEMENTATION // Keep this (Don't define it again elsewhere when including stb_image).
+#include <stb_image.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h> // Must be included after glad.
+#include <glm/glm.hpp>
+
 int main()
 {
 #ifndef NDEBUG
     // Keep this.
-    // This make the executable behave like it's in the assets folder.
-    // Which make assets loading easier.
+    // This makes the executable behave like it's in the assets directory.
+    // Which makes assets loading easier.
     std::filesystem::current_path(std::filesystem::path(PROJECT_ROOT) / "assets");
 #endif
 
